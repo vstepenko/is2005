eedaa73c9a26393ae6aee4f7d5ddbf7447c3cebc
vstep <vstepenko@alliancedigital.tech>
2025-02-25 10:55:00 +0200

    first commit

---
+++ src/main/java/ua/edu/duan/is2005/Is2005Application.java
@@ -0,0 +1,13 @@
+package ua.edu.duan.is2005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is2005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is2005Application.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/duan/is2005/Is2005ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.duan.is2005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is2005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
